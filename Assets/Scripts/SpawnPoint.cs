3a941f5ab200bca5213df74f03b354cd4059552e
STUDENT-PC\Student <s22309617@gmail.com>
2022-07-08 10:40:17 +0800

    chore :start

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tanks
+{
+    public class SpawnPoint : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Awake()
+        {
+            gameObject.SetActive(false);
+        }
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
